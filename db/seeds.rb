a38379093b3a63c034a151b1aa3e6b57ae0c88e6
Brocier <jschoe04@gmail.com>
2018-02-20 11:42:15 -0500

    jar jar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 Creature.create({name: "Luke", description: "Jedi"})
 Creature.create({name: "Darth Vader", description: "Father of Luke"})
+Creature.create({name: "Jar Jar Binks", description: "The most important character"})
